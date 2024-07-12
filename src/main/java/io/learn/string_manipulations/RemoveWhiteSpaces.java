d9095c6290e45a8db1d22bb5c520a2b9c760fda6
nikhilmarathe89 <niksworld975@yahoo.com>
2024-07-12 12:51:44 +0530

    RemoveWhiteSpaces_Commit

---
+++ src/main/java/io/learn/string_manipulations/RemoveWhiteSpaces.java
@@ -0,0 +1,10 @@
+package io.learn.string_manipulations;
+
+public class RemoveWhiteSpaces 
+{
+	public static void main(String[] args) 
+	{
+		String str4 = "Hello World";
+		System.out.println(str4.replace(" ", ""));
+	}
+}
