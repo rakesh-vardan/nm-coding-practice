6854dd059f95e05018d93e56b04a2a4f97ab641a
nikhilmarathe89 <niksworld975@yahoo.com>
2024-07-12 12:54:44 +0530

    ConcatTwoStrings_Commit

---
+++ src/main/java/io/learn/string_manipulations/ConcatTwoStrings.java
@@ -0,0 +1,11 @@
+package io.learn.string_manipulations;
+
+public class ConcatTwoStrings 
+{
+	public static void main(String[] args) 
+	{
+		String str5 = "Hello";
+		String str6 = "World";
+		System.out.println(str5.concat(str6));
+	}
+}
