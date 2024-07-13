41230de62eba9f5fb320ad9966b9076da459c7c8
nikhilmarathe89 <niksworld975@yahoo.com>
2024-07-13 12:42:52 +0530

    AverageOfAnArray_commit

--- src/main/java/io/learn/arrays/AverageOfAnArray.java
+++ src/main/java/io/learn/arrays/AverageOfAnArray.java
@@ -2,17 +2,19 @@ package io.learn.arrays;
 
 import java.util.Arrays;
 
-public class SumOfElementsInAnArray 
+public class AverageOfAnArray 
 {
 	public static void main(String[] args) 
 	{
 		int arr[] = {1,2,3,4,5};
-		int sum=0;
+		int add=0;
 		System.out.println("Original array is: " + Arrays.toString(arr));
 		for(int i=0; i<arr.length;i++)
 		{
-			sum = sum + arr[i];
+			add = add + arr[i];
 		}
-		System.out.println("Sum of given array is: " + sum);
+		float avg;
+		avg = add/5;
+		System.out.println("Average of given array is: " + avg);
 	}
 }
