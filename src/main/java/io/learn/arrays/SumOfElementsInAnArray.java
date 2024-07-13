135d50a8b3ee305d452381c050ad55c8e55169ee
nikhilmarathe89 <niksworld975@yahoo.com>
2024-07-13 12:33:46 +0530

    SumOfAllElementsInArray_commit

---
+++ src/main/java/io/learn/arrays/SumOfElementsInAnArray.java
@@ -0,0 +1,18 @@
+package io.learn.arrays;
+
+import java.util.Arrays;
+
+public class SumOfElementsInAnArray 
+{
+	public static void main(String[] args) 
+	{
+		int arr[] = {1,2,3,4,5};
+		int fact=1;
+		System.out.println("Original array is: " + Arrays.toString(arr));
+		for(int i=0; i<arr.length;i++)
+		{
+			fact = fact * arr[i];
+		}
+		System.out.println("Factorial of given array is: " + fact);
+	}
+}
