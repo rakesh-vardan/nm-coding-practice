3bd9fcfcdfbdbc486e18dd69fa883a3014f43cfa
nikhilmarathe89 <niksworld975@yahoo.com>
2024-07-13 16:11:07 +0530

    ValuePresentInArray_commit

---
+++ src/main/java/io/learn/arrays/ValuePresentInArray.java
@@ -0,0 +1,16 @@
+package io.learn.arrays;
+
+public class ValuePresentInArray 
+{
+	public static void main(String[] args) 
+	{
+		int arr[] = {1,2,3,4,5};
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i] == 3)
+			{
+				System.out.println("Value is present in the array");
+			}
+		}
+	}
+}
