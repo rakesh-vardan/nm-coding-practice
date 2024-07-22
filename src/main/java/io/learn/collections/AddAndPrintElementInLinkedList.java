d1c97429f203c55a0b90ec2599718b686eb1578d
nikhilmarathe89 <niksworld975@yahoo.com>
2024-07-22 13:09:50 +0530

    AddAndPrintElementInLinkedList_commit

--- src/main/java/io/learn/collections/AddAndPrintElementInLinkedList.java
+++ src/main/java/io/learn/collections/AddAndPrintElementInLinkedList.java
@@ -1,26 +1,26 @@
 package io.learn.collections;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-public class ReadElementsInArrayList 
+public class AddAndPrintElementInLinkedList 
 {
 	public static void main(String[] args) 
 	{
-		List<String> color = new ArrayList<String>();
+		List<String> color = new LinkedList<String>();
 		color.add("Red");
 		color.add("Green");
 		color.add("Blue");
-		System.out.print("\"");
+		System.out.print("[");
 		for(int i=0; i<color.size();i++)
 		{
 			if(i== color.size()-1)
 			{
-				System.out.print(color.get(i)+"\"");
+				System.out.print("\"" + color.get(i)+"\"" + "]");
 			}
 			else
 			{
-				System.out.print(color.get(i)+ " ");
+				System.out.print("\"" + color.get(i)+"\"," + " ");
 			}
 		}
 	}
